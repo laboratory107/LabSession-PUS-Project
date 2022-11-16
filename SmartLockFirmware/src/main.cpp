ec5ef2a05da8b6857358e53480a35f914aba73e0
Boris Tomas <boris.tomas@gmail.com>
2022-11-16 09:25:50 +0100

    Merge Touch

--- SmartLockFirmware/src/main.cpp
+++ SmartLockFirmware/src/main.cpp
@@ -1,18 +1,26 @@
 #include <SPI.h>
 #include <SDLogger.h>
 #include <WebServer.h>
+#include "TouchControl.h"
+
+SDLogger sdLog;
+WebServer server;
+TouchControl touchControl;
 
 long currentTime;
 String loopMessage = "";
-SDLogger sdLog;
-WebServer server;
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(2000);
   Serial.println("");
   Serial.println("Lock start");
+  touchControl.begin(T0);
+  touchControl.addToDictionary('S', 100, 1000, true);
+  touchControl.addToDictionary('L', 1000, 5000, true);
+  touchControl.addToDictionary('P', 100, 1000, false);
+  touchControl.registerPattern("SPSPL");
 
   if (!sdLog.Begin("/log.txt"))
   {
@@ -26,6 +34,13 @@ void setup()
 }
 void loop()
 {
+	bool patternFound = touchControl.checkPattern();
+
+  if (patternFound){
+    Serial.printf("Successful pattern %s \n", touchControl.getLastPattern());
+    //Serial.println("unlocked");
+  }
+  
   currentTime = millis();
   loopMessage = String(currentTime);
   loopMessage.concat(" loop");
