3adbe688f14767070ef23d08323a61e0145d427f
Elvis Popović <elvis_popovic@yahoo.com>
2022-11-24 22:36:48 +0100

    OLED support added

--- SmartLockFirmware/src/main.cpp
+++ SmartLockFirmware/src/main.cpp
@@ -1,18 +1,25 @@
 #include <SPI.h>
 #include <SDLogger.h>
 #include <WebServer.h>
+#include <oled.h>
 #include <TouchControl.h>
 
 SDLogger sdLog;
 WebServer server;
 TouchControl touchControl;
 
+OLED *oled;
+
 void setup()
 {
   Serial.begin(115200);
+  oled = new OLED(128,64,0x3C); 
   delay(2000);
   Serial.println("");
   Serial.println("Lock start");
+  oled->drawMessage("Lock start");
+  delay(1000);
+
   touchControl.begin(T0);
   touchControl.addToDictionary('S', 100, 1000, true);
   touchControl.addToDictionary('L', 1000, 5000, true);
@@ -21,6 +28,8 @@ void setup()
 
   if (!sdLog.begin("/log.txt"))
   {
+    oled->drawError("No SD log.");
+    delay(1000);
     return;
   }
 
@@ -28,11 +37,21 @@ void setup()
   server.start();
 
   sdLog.writeLine("Setup complete");
+  oled->drawMessage("Setup complete.");
+  delay(1000);
 }
 void loop()
 {
-	if (touchControl.checkPattern()){
+	if (touchControl.checkPattern())
+  {
     Serial.printf("Successful pattern %s \n", touchControl.getLastPattern());
     sdLog.writeLine("Pattern found - unlocked");
+    oled->drawAccess(true);
+    delay(5000);
+  }
+  else
+  {
+    oled->drawAccess(false);
+    delay(1000);
   }
 }
